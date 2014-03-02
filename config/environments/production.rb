468f50d683f1ab83e3df70e9d620f9209f7af0fe
princesspretzel <brittanga@gmail.com>
2014-03-02 15:57:10 -0500

    turned precompiles into array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,7 @@ Shark::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += 'game.js'
-  config.assets.precompile += 'users.css'
+  config.assets.precompile += %w( game.js users.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
